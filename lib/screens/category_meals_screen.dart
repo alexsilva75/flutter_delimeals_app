908332a82d975253b1d7423ba2009ac2602d4e7d
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2020-07-10 11:23:24 -0300

    Listing category meals; Showing meals by category with image; dummy_data.dart was updated

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:my_meals_app/widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
+    //print('Category Id from routeArgs: ' + categoryId);
+
+    final categoryMeals = DUMMY_MEALS.where(
+      (meal) {
+        //print('Meal categorires' + meal.categories);
+        //meal.categories
+
+        return meal.categories.contains(categoryId);
+      },
+    ).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
